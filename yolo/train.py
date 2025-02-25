a6b37d23e26f8dc856751fc61867e70bc6017118
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-02-24 20:54:32 -0400

    feat: added steel_bot_to_train.ipynb script
    * Added steel_bot_to_train.ipynb script
    * Improved zip and train script

--- yolo/train.py
+++ yolo/train.py
@@ -1,18 +1,22 @@
 from ultralytics import YOLO
 
-if __name__ == '__main__':
+# Train model
+def train_model(yolo_path='yolo11n.pt', device='cpu', data='data.yaml', epochs=100, imgsz=640, project='yolo', name='model'):
     # Load a model
-    model = YOLO("./yolo/yolo11n.pt")
+    model = YOLO(yolo_path)
 
     # Train the model
     train_results = model.train(
-        data="./yolo/data.yaml",
-        epochs=100,
-        imgsz=640,
-        device="cpu",
-        project="./yolo/runs",
-        name="steel-bot"
+        data=data,
+        epochs=epochs,
+        imgsz=imgsz,
+        device=device,
+        project=project,
+        name=name,
     )
 
     # Export the model to ONNX format
-    path = model.export(format="onnx")  # return path to exported model
+    path = model.export(format="onnx")
+
+    # Log
+    print(f"Model exported to {path}")
--- yolo/zip.py
+++ yolo/zip.py
@@ -2,10 +2,14 @@ import os
 import zipfile
 
 # Define the function to zip the files in a folder
-def zip_files(zipf, files, input_file_base_path: str, input_base_path:str):
-    for file in files:
+def zip_files(zipf, filenames, input_file_base_path: str, input_base_path:str, ignore_filenames:list = None):
+    for filename in filenames:
+        # Skip the file if it is in the ignore list
+        if ignore_filenames is not None and filename in ignore_filenames:
+            continue
+
         # Zip the file
-        file_path = os.path.join(input_file_base_path, file)
+        file_path = os.path.join(input_file_base_path, filename)
         file_rel_path = os.path.relpath(file_path, input_base_path)
         zipf.write(file_path, file_rel_path)
 
@@ -13,31 +17,34 @@ def zip_files(zipf, files, input_file_base_path: str, input_base_path:str):
         print(f'Zipped file: {file_rel_path}')
 
 # Define the function to zip a folder, this ignores nested folders
-def zip_not_nested_folder(zipf, input_folder_path:str, input_base_path:str):
+def zip_not_nested_folder(zipf, input_folder_path:str, input_base_path:str, ignore_filenames:list=None):
     # Get the list of files in the specified folder
-    files = [f for f in os.listdir(input_folder_path)]
+    filenames = [f for f in os.listdir(input_folder_path)]
 
     # Zip the files in the folder
-    zip_files(zipf, files, input_folder_path, input_base_path)
+    zip_files(zipf, filenames, input_folder_path, input_base_path, ignore_filenames)
 
     # Log
     input_folder_rel_path = os.path.relpath(input_folder_path, input_base_path)
     print(f'Zipped folder: {input_folder_rel_path}')
 
 # Define the function to zip a folder, this includes nested folders
-def zip_nested_folder(zipf, input_base_path:str, input_folder_path:str):
-    for root, dirs, files in os.walk(input_folder_path):
+def zip_nested_folder(zipf, input_base_path:str, input_folder_path:str, ignore_filenames:list=None):
+    for root, _, filenames in os.walk(input_folder_path):
         # Zip the files in the subfolders
-        zip_files(zipf, files, root, input_base_path)
+        zip_files(zipf, filenames, root, input_base_path, ignore_filenames)
 
         # Log
-        print(f'Zipped folder: {root} from {input_base_path}')
+        input_folder_rel_path = os.path.relpath(input_folder_path, input_base_path)
+        print(f'Zipped folder: {input_folder_rel_path}')
 
 # Define the function to zip the required files for model training
-def zip_to_train(input_yolo_dir:str, input_yolo_dataset_organized_to_process_dir:str, output_zip:str):
-    with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
+def zip_to_train(input_yolo_dir:str, input_yolo_dataset_organized_to_process_dir:str, output_zip_dir:str, output_zip_filename:str):
+    output_zip_path=os.path.join(output_zip_dir, output_zip_filename)
+
+    with zipfile.ZipFile(output_zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
         # Zip the YOLO files
-        zip_not_nested_folder(zipf, input_yolo_dir, input_yolo_dir)
+        zip_not_nested_folder(zipf, input_yolo_dir, input_yolo_dir, [output_zip_filename])
 
         # Zip the YOLO dataset organized files
         zip_nested_folder(zipf, input_yolo_dir, input_yolo_dataset_organized_to_process_dir)
@@ -48,6 +55,7 @@ if __name__ == '__main__':
     base_dir = os.path.join(cwd, 'yolo')
     input_yolo_dir = os.path.join(base_dir)
     input_yolo_dataset_organized_to_process_dir = os.path.join(base_dir, 'dataset', 'organized', 'to_process')
-    output_zip = os.path.join(base_dir, 'steel_bot_to_train.zip')
+    output_zip_dir=base_dir
+    output_zip_filename = 'steel_bot_to_train.zip'
 
-    zip_to_train(input_yolo_dir, input_yolo_dataset_organized_to_process_dir, output_zip)
+    zip_to_train(input_yolo_dir, input_yolo_dataset_organized_to_process_dir, output_zip_dir, output_zip_filename)
